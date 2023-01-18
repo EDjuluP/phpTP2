a9a7000abfca1b684e0397728696c23e5c0f187a
EDjuluP <emmanuel.djulupenghe@gmail.com>
2023-01-18 16:49:05 +0100

    Exo1_TP2

--- exo1.php
+++ exo1.php
@@ -1,1 +1,44 @@
-<?php echo("coucou"); ?>
+
+
+<?php echo("Bonjour !"); 
+
+function MaFonction(){
+    ?>
+       
+<table style="border: 1px solid #333;">
+    <th>fruit</th>
+    <th>legumes</th>
+    <th>autres</th>
+
+    <tr>
+        <td>Pomme</td>
+        <td>Aubergine</td>
+        <td>Bol</td>
+    </tr>
+
+    <tr>
+        <td>Orange</td>
+        <td>Tomate</td>
+        <td>Saladier</td>
+    </tr>
+
+    <tr>
+        <td>Banane</td>
+        <td>Tomate</td>
+        <td>Moule</td>
+    </tr>
+</table>
+<?php }?>
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+MaFonction();
+?>
+
+</body>
+</html> 
--- exo2.php
+++ exo2.php
@@ -1,1 +1,44 @@
-<?php echo("Je suis Emmanuel"); ?>
+
+<?php echo("Bonjour !"); 
+
+function MaFonction($colone1,$colone2,$colone3){
+    ?>
+       
+<table style="border: 1px solid #333;">
+    <th><?=$colone1?></th>
+    <th><?=$colone2?></th>
+    <th><?=$colone3?></th>
+
+    <tr>
+        <td>Pomme</td>
+        <td>Aubergine</td>
+        <td>Bol</td>
+    </tr>
+
+    <tr>
+        <td>Orange</td>
+        <td>Tomate</td>
+        <td>Saladier</td>
+    </tr>
+
+    <tr>
+        <td>Banane</td>
+        <td>Tomate</td>
+        <td>Moule</td>
+    </tr>
+</table>
+<?php }?>
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+MaFonction("fruit","legume","autres");
+MaFonction("nom","prenoms","age");
+?>
+
+</body>
+</html> 
